1522237e0bda8e95d6a3a802691bfebb37423418
yukunfeng <yukunfg@gmail.com>
2018-07-04 22:44:10 +0900

    train.py

--- train.py
+++ train.py
@@ -9,7 +9,11 @@ Description : training
 
 import argparse
 import opts
+import torch.nn as nn
+import torch.optim as optim
 from utils.utils import get_logger
+from nnlm import NNLM
+import dataset
 
 
 def parse_args():
@@ -24,6 +28,32 @@ def parse_args():
     #  torch.manual_seed(opt.seed)
 
     return opt
+
+
+def train(opt):
+    """training given opt"""
+    model = NNLM(
+        rnn_type=opt.rnn_type,
+        bidirectional=opt.bidirectional,
+        num_layers=opt.num_layers,
+        vocab_size=opt.vocab_size,
+        word_dim=opt.word_dim,
+        hidden_size=opt.hidden_size,
+        dropout=opt.dropout
+    )
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr = 0.01)
+    train_iter, test_iter, val_iter = dataset.create_lm_dataset()
+    for batch_train in train_iter:
+        optimizer.zero_grad()
+        text, target = batch_train.text, batch_train.target
+        prediction = model(text)
+        loss = criterion(
+            prediction.view(-1, model.vocab_size),
+            target.view(-1)
+        )
+        loss.backward()
+        optimizer.step()
 
 
 if __name__ == "__main__":
